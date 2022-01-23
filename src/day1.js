6373047bb770d02c3616226a98e689214b6fbd45
Chris Soper <soperc@corning.com>
2022-01-23 11:13:37 -0500

    Second initial commit :)

---
+++ src/day1.js
@@ -0,0 +1,13 @@
+/**
+ * Started here:  https://threejs.org/docs/index.html#manual/en/introduction/Installation
+ */
+import {
+  Scene
+} from 'three';
+
+const scene = new Scene();
+
+export default function main() {
+  return "Hello";
+}
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * This is the entrypoint for the module.  For now lets avoid doing stuff in here directly,
+ * and instead reference outside from here.
+ */
+import main from './day1';
+
+main();
