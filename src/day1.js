d089ae4730f48727d1fbc19cd01f41b2ce988619
Chris Soper <soperc@corning.com>
2022-01-23 11:42:15 -0500

    we have a cube

--- src/day1.js
+++ src/day1.js
@@ -2,35 +2,57 @@
  * Started here:  https://threejs.org/docs/index.html#manual/en/introduction/Installation
  */
 import {
+  BoxGeometry,
+  MeshBasicMaterial,
   Scene,
+  Mesh,
   PerspectiveCamera,
   OrthographicCamera,
   WebGLRenderer
 } from 'three';
 
 
-export default function main() {
-  const scene = new Scene();
+let scene, camera;
+const renderer = new WebGLRenderer();
 
-  let cameraWidth = 200;
-  let cameraHeight = 200;
+function setStage() {
+  scene = new Scene();
+  const cameraWidth = 200, cameraHeight = 200;
 
-  const camera = new OrthographicCamera(
-    cameraWidth / -2,
-    cameraWidth / 2,
-    cameraHeight / 2,
-    cameraHeight / -2,
-    1,
+  // const camera = new OrthographicCamera(
+  //   cameraWidth / -2,
+  //   cameraWidth / 2,
+  //   cameraHeight / 2,
+  //   cameraHeight / -2,
+  //   1,
+  //   1000
+  // );
+  camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
     1000
   );
-
-  const renderer = new WebGLRenderer();
-  let rendererMargin = 50;
-  renderer.setSize(
-    window.innerWidth - rendererMargin,
-    window.innerHeight - rendererMargin
-  );
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 }
 
+export default function main() {
+  setStage();
+  camera.position.z = 5;
+  scene.add(generateCube());
+  animate();
+}
+
+function generateCube() {
+  return new Mesh(
+    new BoxGeometry(),
+    new MeshBasicMaterial({color: 0x00ff00})
+  );
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
