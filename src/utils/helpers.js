77328ab8d83a6b6a4979b55caf4c14e5834d046d
Chris Soper <soperc@corning.com>
2022-02-12 13:53:40 -0500

    updated helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
-import {OrthographicCamera, WebGLRenderer} from "three/src/Three";
+import {AudioLoader, OrthographicCamera, PerspectiveCamera, WebGLRenderer} from "three/src/Three";
+import {AudioAnalyser} from "../SonicVisualizer/AudioAnalyzer";
 
 export function getDefaultRenderer() {
   let renderer = new WebGLRenderer({antialias: true});
@@ -15,4 +16,37 @@ export function getDefaultCamera() {
   camera.updateProjectionMatrix();
   camera.lookAt(0, 0, 0);
   return camera;
+}
+
+export function getPerspectiveCamera() {
+  let camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 10, 0);
+  camera.lookAt(0, 0, 0);
+  return camera
+}
+
+/**
+ * Load an audio file into a context and attach a new analyzer
+ *
+ * @param audioFile - audio file path
+ * @param audioContext {BaseAudioContext}
+ * @param fftSize {number}
+ * @returns {Promise<{analyzer: AudioAnalyser, context: BaseAudioContext, source: *}>}
+ */
+export function loadAudio(audioFile, audioContext = new AudioContext(), fftSize = 1024) {
+  const loader = new AudioLoader();
+  const analyser = new AudioAnalyser(audioContext, fftSize);
+  return new Promise(resolve => loader.load(audioFile, resolve))
+    .then(buffer => {
+      const source = audioContext.createBufferSource();
+      source.buffer = buffer;
+      source.connect(audioContext.destination);
+      source.connect(analyser.analyser);
+      // source.start(0);  Maybe hold off on this?
+      return {
+        'source': source,
+        'context': audioContext,
+        'analyzer': analyser,
+      };
+    });
 }
