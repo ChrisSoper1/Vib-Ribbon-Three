5805c5e96fafff869cf3443fba359f3ad435a6a4
Chris Soper <soperc@corning.com>
2022-02-15 11:47:01 -0500

    Geometry in visualizer 5 is working

--- src/SonicVisualizer/Visualizer5.js
+++ src/SonicVisualizer/Visualizer5.js
@@ -12,6 +12,9 @@ import {
   BufferAttribute,
   PlaneGeometry,
   Mesh,
+  WireframeGeometry,
+  DoubleSide,
+  LineSegments,
   ConeGeometry,
   Group,
   OrthographicCamera,
@@ -21,10 +24,14 @@ import {
   BufferGeometry,
   PointsMaterial,
   MeshPhongMaterial,
+  MeshLambertMaterial,
+  StaticReadUsage,
+  StreamDrawUsage, LineBasicMaterial, Float32BufferAttribute,
 } from "three/src/Three";
 
 import {Lut} from "three/examples/jsm/math/Lut";
 import Stats from 'three/examples/jsm/libs/stats.module.js';
+import {VertexNormalsHelper} from 'three/examples/jsm/helpers/VertexNormalsHelper.js';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 import {AudioAnalyser} from './AudioAnalyzer';
@@ -38,22 +45,17 @@ export class SonicVisualizer5 {
 
   constructor(fftSize = 128) {
     this.fftSize = 128;
+    this.gridSize = this.fftSize / 2;
+
     this.stats = new Stats();
     document.body.appendChild(this.stats.dom);
     this.renderer = getDefaultRenderer();
     // this.camera = getDefaultCamera();
-    // this.camera = getPerspectiveCamera();
-    this.camera = new OrthographicCamera(
-      -25,
-      this.fftSize / 1.5,
-      this.fftSize / 1.5,
-      -25,
-      this.fftSize * -1,
-      this.fftSize,
-    );
-    this.camera.position.set(0, 0, -100);
+    this.camera = getPerspectiveCamera();
+    this.camera.position.z = 128;
     this.camera.updateProjectionMatrix();
-    this.camera.lookAt(100, fftSize / 4, 0);
+
+    this.camera.lookAt(this.gridSize, this.gridSize, 0);
     this.scene = new Scene();
     const axesHelper = new AxesHelper(5);
     this.scene.add(axesHelper);
@@ -65,8 +67,9 @@ export class SonicVisualizer5 {
     this.audioContext = new AudioContext();
     this.analyser = null;
 
-    this.grid, this.colorAttrGrid = null; // Set below
-    this.generateGrid(this.fftSize / 2);
+    this.colorAttr = null; // Set below
+    this.geometry = null;
+    this.generateGrid(this.gridSize);
 
     document.body.appendChild(this.renderer.domElement);
     window.addEventListener('resize', () => this.renderer.setSize(window.innerWidth, window.innerHeight));
@@ -82,40 +85,85 @@ export class SonicVisualizer5 {
   }
 
   generateGrid(gridSize) {
-    const outputGrid = [];
-    const colorAttrGrid = [];
     // let _material = new MeshBasicMaterial({
-    let _material = new MeshBasicMaterial({
+    let _material = new MeshLambertMaterial({
+      // side: DoubleSide,
       color: 0xffffff,
       vertexColors: true,
     });
-    // let _proto_geom = new PlaneGeometry(1, 1);
-    // let _proto_geom = new BoxGeometry(1, 1, 1);
-    let _proto_geom = new ConeGeometry(1, 7, 32);
-    let _vertexes = _proto_geom.attributes.position.count;
-    _proto_geom.setAttribute('color', new BufferAttribute(new Float32Array(_vertexes * 3), 3));
 
-    for (let rowIx = 0; rowIx < gridSize; rowIx++) {
+    let _positionAttr = new Float32BufferAttribute(new Float32Array(gridSize * gridSize * 3), 3);
+    _positionAttr.setUsage(StaticReadUsage);
 
-      let rowGroup = [];
-      let colorGroup = [];
+    let colorAttr = new Float32BufferAttribute(new Float32Array(gridSize * gridSize * 3), 3);
+    colorAttr.setUsage(StreamDrawUsage);
 
-      for (let colIx = 0; colIx < gridSize; colIx++) {
-        let geometry = _proto_geom.clone();
-        colorGroup.push(geometry.attributes.color);
-
-        let mesh = new Mesh(geometry, _material);
-        rowGroup.push(mesh);
-
-        this.scene.add(mesh);
-        mesh.position.set(colIx * 5, 0, rowIx * 5);
-        // mesh.lookAt(this.camera.position);
+    let rowStartIx;
+    for (let i = 0; i <= gridSize; i++) {
+      rowStartIx = gridSize * i;
+      for (let j = 0; j <= gridSize; j++) {
+        _positionAttr.setXYZ(
+          rowStartIx + j,
+          j * 2.0,
+          i * 2.0,
+          0,
+        );
       }
-      outputGrid.push(rowGroup);
-      colorAttrGrid.push(colorGroup);
     }
-    this.grid = outputGrid;
-    this.colorAttrGrid = colorAttrGrid;
+    _positionAttr.needsUpdate = true;
+
+    // This is stupid.  See https://github.com/mrdoob/three.js/blob/master/examples/webgl_buffergeometry_indexed.html
+    let indices = [];
+    for (let i = 0; i < gridSize - 1; i++) {
+      for (let j = 0; j < gridSize - 1; j++) {
+        /*
+         * This defines what vertexes connect to each other to make a
+         * triangle pattern which defines a mesh surface.
+         *
+         * The indexes of vertexes in the window use the following variables
+         * | b | a |
+         * | d | c |
+         * Triangle 1 is vertexes (b,a,d)
+         * Triangle 2 is vertexes (b,d,c)
+         * change switch the second and third vertex in both triangles to face the other direction
+         */
+        const a = i * gridSize + (j + 1);
+        const b = i * gridSize + j;
+        const c = (i + 1) * gridSize + j;
+        const d = (i + 1) * gridSize + (j + 1);
+        indices.push(b, a, d); // triangle one
+        indices.push(b, d, c); // triangle two
+      }
+    }
+
+    let geometry = new BufferGeometry();
+    geometry.setIndex(indices);
+    geometry.setAttribute('position', _positionAttr);
+    geometry.setAttribute('color', colorAttr);
+    // geometry.lookAt(this.camera.position); // TODO! Uses this.camera
+
+    geometry.computeVertexNormals();
+    geometry.computeBoundingBox();
+    geometry.computeBoundingSphere();
+
+    // const wireframe = new WireframeGeometry(geometry);
+    // const line = new LineSegments(
+    //   wireframe,
+    //   new LineBasicMaterial({
+    //     depthTest: false,
+    //     opacity: 0.25,
+    //     transparent: true,
+    //   }),
+    // );
+    // this.scene.add(line);
+
+    let mesh = new Mesh(geometry, _material);
+    this.scene.add(mesh);
+    const helper = new VertexNormalsHelper(mesh, 5, 0xFFFFFF, 1);
+    this.scene.add(helper);
+
+    this.geometry = geometry;
+    this.colorAttr = colorAttr;
   }
 
   animate() {
@@ -142,30 +190,39 @@ export class SonicVisualizer5 {
   }
 
   updateColors() {
+    let colorAttr = this.colorAttr; // in case I want to refactor this method to be shared
+
+    // TODO: this could could be improved by refactoring it as a transform.
+    //  additionally it can be implemented in the shader
+
     // move rows back
-    for (let i = 2; i < 0; i--) {
-      // update the colors
-      for (let j = 0; j < this.colorAttrGrid[i].length; j++) {
-        let destinationAttr = this.colorAttrGrid[i][j];
-        let sourceAttr = this.colorAttrGrid[i - 1][j];
+    /*
+     for (let i = 0; i < this.gridSize - 1; i++) {
+     // Start with the first item of the last row
+     let targetIx = (this.gridSize * this.gridSize - 1) - (this.gridSize * (i - 1));
+     let sourceIx = targetIx - this.gridSize;
+     for (let j = 0; j < this.gridSize; j++) {
+     colorAttr.setXYZ(
+     targetIx + j,
+     colorAttr.getX(sourceIx + j),
+     colorAttr.getY(sourceIx + j),
+     colorAttr.getZ(sourceIx + j),
+     );
+     }
+     }
+     */
 
-        destinationAttr.copy(sourceAttr);
-        destinationAttr.needsUpdate = true;
-      }
-    }
-    // update first row
-    let mostRecent = this.colorAttrGrid[0];
+    // update all rows
     let data = this.analyser.getFrequencyData();
-
-    for (let j = 0; j < mostRecent.length; j++) {
-      if (data.length) {
-        let attr = mostRecent[j];
-        let targetColor = this.colorMap.getColor(data[j]);
-        for (let vertexIx = 0; vertexIx < attr.count; vertexIx++) {
-          attr.setXYZ(vertexIx, targetColor.r, targetColor.g, targetColor.b);
+    for (let j = 0; j < this.gridSize; j++) {
+      for (let i = 0; i < this.gridSize; i++) {
+        if (data.length) {
+          let targetColor = this.colorMap.getColor(data[i]);
+          colorAttr.setXYZ(j * this.gridSize + i, targetColor.r, targetColor.g, targetColor.b);
         }
-        attr.needsUpdate = true;
       }
     }
+
+    colorAttr.needsUpdate = true;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@
  * This is the entrypoint for the module.  For now lets avoid doing stuff in here directly,
  * and instead reference outside from here.
  */
-// import {VibRibbonApplication} from './day2';
+import {VibRibbonApplication} from './day2';
 // const app = new VibRibbonApplication();
-
-// import {SonicVisualizer2} from './SonicVisualizer/Visualizer2';
-// import {SonicVisualizer3} from './SonicVisualizer/Visualizer3';
-// import {SonicVisualizer4} from './SonicVisualizer/Visualizer4';
-import {SonicVisualizer5} from './SonicVisualizer/Visualizer5';
 
 const audioFile = './Sam_and_Dave-Hold_on_Im_coming.mp3';
 // const audioFile = './A History of Bad Men.mp3';
 
+// import {SonicVisualizer2} from './SonicVisualizer/Visualizer2';
 // const app = new SonicVisualizer2();
+// import {SonicVisualizer3} from './SonicVisualizer/Visualizer3';
 // const app = new SonicVisualizer3(audioFile);
+// import {SonicVisualizer4} from './SonicVisualizer/Visualizer4';
 // const app = new SonicVisualizer4(audioFile);
+
+import {SonicVisualizer5} from './SonicVisualizer/Visualizer5';
+
 const app = new SonicVisualizer5();
 app.start(audioFile);
