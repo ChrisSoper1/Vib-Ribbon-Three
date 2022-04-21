bcc956f679d14ea455caed3ff3c70bf6c7b3c36d
Chris Soper <soperc@corning.com>
2022-04-21 14:16:09 -0400

    Added some level related stuff, hope I didn't break anything

--- src/levels/index.js
+++ src/levels/index.js
@@ -1,95 +1,25 @@
-import {
-  BufferGeometry,
-  Line,
-  LineBasicMaterial,
-  Vector3,
-} from "three/src/Three";
-import {MeshBasicMaterial} from "three";
+export * from './geometry';
 
+export class Feature {
+  geometry_type = null;
+  inputs_required = null;
+  time = null;
+  animation = null;
+  input_window_length = 100;
+}
 
-export const featureWidth = 15;
-export const featureHeight = 20;
-let waveWidthStep = featureWidth / 8;
-let widthStep = featureWidth / 8;
-let heightStep = featureHeight / 8;
+/**
+ * This would make a lot of sense if it was implemented as an iterable, which features could be pushed and popped.
+ */
+export class Level {
+  song = null;
+  /** @type {Feature[]} */
+  features = [];
+  _index = [];
+  geometry = [];
 
-export const LINE = new Line(
-  new BufferGeometry().setFromPoints([
-    new Vector3(0, 0, 0),
-    new Vector3(featureWidth, 0, 0),
-  ]),
-  new LineBasicMaterial({color: 0xFFFFFF}),
-);
-
-export const BLOCK = new Line(
-  new BufferGeometry().setFromPoints([
-    new Vector3(0, 0, 0),
-    new Vector3(0, featureHeight, 0),
-    new Vector3(featureWidth, featureHeight, 0),
-    new Vector3(featureWidth, 0, 0),
-  ]),
-  new LineBasicMaterial({color: 0xFFFFFF}),
-);
-
-export const PIT = new Line(
-  new BufferGeometry().setFromPoints([
-    new Vector3(0, 0, 0),
-    new Vector3(featureWidth / 2, featureHeight * -1, 0),
-    new Vector3(featureWidth, 0, 0),
-  ]),
-  new LineBasicMaterial({color: 0xFFFFFF}),
-);
-
-/** This is incomplete **/
-export const LOOP = new Line(
-  new BufferGeometry().setFromPoints([
-    new Vector3(0, 0, 0),
-    new Vector3(widthStep * 6, heightStep * 1, 0),
-    new Vector3(widthStep * 7, heightStep * 2, 0),
-    new Vector3(widthStep * 7, heightStep * 3, 0),
-    new Vector3(widthStep * 6, heightStep * 4.2, 0),
-    new Vector3(widthStep * 5, heightStep * 4.4, 0),
-    new Vector3(widthStep * 4, heightStep * 4.6, 0),
-    new Vector3(widthStep * 3, heightStep * 4.4, 0),
-    new Vector3(widthStep * 2, heightStep * 4.2, 0),
-    new Vector3(widthStep * 1, heightStep * 3, 0),
-    new Vector3(widthStep * 1, heightStep * 2, 0),
-    new Vector3(widthStep * 2, heightStep * 1, 0),
-    new Vector3(featureWidth, 0, 0),
-  ]),
-  new LineBasicMaterial({color: 0xFFFFFF}),
-);
-
-export const WAVE = new Line(
-  new BufferGeometry().setFromPoints([
-    new Vector3(0, 0, 0),
-    new Vector3(waveWidthStep * 1, featureHeight / 3, 0),
-    new Vector3(waveWidthStep * 2, featureHeight / -3, 0),
-    new Vector3(waveWidthStep * 3, featureHeight / 3, 0),
-    new Vector3(waveWidthStep * 4, featureHeight / -3, 0),
-    new Vector3(waveWidthStep * 5, featureHeight / 3, 0),
-    new Vector3(waveWidthStep * 6, featureHeight / -3, 0),
-    new Vector3(waveWidthStep * 7, featureHeight / 3, 0),
-    new Vector3(featureWidth, 0, 0),
-  ]),
-  new LineBasicMaterial({color: 0xFFFFFF}),
-);
-
-export const BLOCKPIT = new Line(
-  new BufferGeometry().setFromPoints([
-    new Vector3(0, 0, 0),
-    new Vector3(0, featureHeight, 0),
-    new Vector3(featureWidth / 2, featureHeight / 2, 0),
-    new Vector3(featureWidth, featureHeight, 0),
-    new Vector3(featureWidth, 0, 0),
-  ]),
-  new LineBasicMaterial({color: 0xFFFFFF}),
-);
-
-const BLOCKLOOP = [];
-const BLOCKWAVE = [];
-
-const PITLOOP = [];
-const PITWAVE = [];
-
-const LOOPWAVE = [];
+  constructor(features) {
+    this.features = features;
+    this._index = Array.from(this.features).map(x => x.time);
+  }
+}
